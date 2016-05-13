6fde114876f0d49d772f84a2cebdf1c4a8352917
jgalgarra <ghostdatalearner@ghostmail.com>
2016-05-13 14:26:18 +0200

    documenting analyze_network

--- R/network-kanalysis.R
+++ R/network-kanalysis.R
@@ -1,32 +1,34 @@
 library(igraph)
 library(bipartite)
 library(ggplot2)
+#' Functions for k-core decompose a bipartite graph and computing some newtork indexes
+
+#' Reads a network interaction matrix from a CSV file
+#'
+#' Args:
+#'   namenetwork: CSV file that contains the interaction matrix
+#'   guild_a, guild_b: Identifier of the guild of speciea of each class. Default "pl" (plant)
+#'                     "pol" (pollinator)
+#'   directory: directory where newtork CSVs are located
+#'
+#' Return List:
+#'   graph: Newtork as an igraph object
+#'   m    : Interaction matrix
+#'   num_guild_b : number of species of guild_b
+#'   num_guild_a" : number of species of guild_a
+#'   names_guild_a : names of nodes of guild_a
+#'   names_guild_b : names of species of guild_b
 
 read_network <- function(namenetwork, guild_astr = "pl", guild_bstr = "pol", directory="")
-  # Reads a network interaction matrix from a CSV file
-  #
-  # Args:
-  #   namenetwork: CSV file that contains the interaction matrix
-  #   guild_a, guild_b: Identifier of the guild of speciea of each class. Default "pl" (plant)
-  #                     "pol" (pollinator)
-  #   directory: directory where newtork CSVs are located
-  #
-  # Return List:
-  #   graph: Newtork as an igraph object
-  #   m    : Interaction matrix
-#   num_guild_b : number of species of guild_b 
-#   num_guild_a" : number of species of guild_a
-#   names_guild_a : names of nodes of guild_a
-#   names_guild_b : names of species of guild_b
 {
   # Reading species names
   namesred <- read.csv(paste0(directory,namenetwork),header=FALSE,stringsAsFactors=FALSE)
   names_guild_a <- namesred[1,2:ncol(namesred)]
   names_guild_b <- namesred[2:nrow(namesred),1]
-  
+
   #Reading matrix data
   m <- read.csv(paste0(directory,namenetwork),header=TRUE,row.names=1)
-  
+
   num_guild_a <- ncol(m)
   num_guild_b <- nrow(m)
   g <- graph.empty()
@@ -43,57 +45,48 @@ read_network <- function(namenetwork, guild_astr = "pl", guild_bstr = "pol", dir
       }
     }
   }
-  calc_values <- list("graph" = g, "matrix" = m, "num_guild_b" = num_guild_b, "num_guild_a" = num_guild_a, 
+  calc_values <- list("graph" = g, "matrix" = m, "num_guild_b" = num_guild_b, "num_guild_a" = num_guild_a,
                       "names_guild_a" = names_guild_a, "names_guild_b"=names_guild_b)
   return(calc_values)
 }
 
-# Cambio JGA
-#
-# randomize_and_write <- function(matrix, namenetwork, rlinks = 0,  directory = "", bypercentage = TRUE)
-# {
-#   if (bypercentage)
-#     filesuf <- paste0("_rnd_",rlinks)
-#   else
-#     filesuf <- paste0("_lnk_",rlinks)
-#   if (rlinks > 0)
-#   {
-#     links <- matrix == 1
-#     nolinks <- matrix == 0
-#     rows <- nrow(matrix)
-#     cols <- ncol(matrix)
-#     if (bypercentage) 
-#       extractions <- round(rlinks*sum(links)/100)
-#     else 
-#       extractions <- rlinks
-#     onestozeroes <- sample(which(links),extractions)
-#     zeroestoones <- sample(which(nolinks),extractions)
-#     for (i in onestozeroes){
-#       matrix[i] = 0
-#     }
-#     for (i in zeroestoones){
-#       matrix[i] = 1
-#     }
-#   }
-#   nfile <- paste0(directory,strsplit(namenetwork,"\\.")[[1]][1],filesuf,".csv")
-#   write.csv(matrix,nfile)
-# }
-#
-# Esta funci???n no hace falta para las graficas, la he llevado a otra libreria
-
-
-
 #' Network analysis using k core decomposition
 #'
-#' 
-#' @param love Do you love cats? Defaults to TRUE.
-#' @keywords cats
+#' This function performs the kcore decomposition of a bipartite network. The input is
+#' the interaction matrix in a .csv file, with the format of  \url{www.web-of-life.es}
+#' Species of guild a are distributed colums, and those of guild b by rows. First
+#' colum contains the labels of guilb b nodes, and first row the labels of guild a.
+#' If the interaction matrix is binary, the cell of species_a_m,species_b_n will be set to 1.
+#' If it is weighted, a real number different of 0.
+#'
+#' @param namenetwork is the file name that contains the interaction matrix
+#' @param directory where the network file is stored
+#' @param guild_a prefix for the guild of nodes stored in rows
+#' @param guild_b prefix for the guild of nodes stored in columns
+#' @param plot_graphs plot kshell histogram and kamada kawai plots
+#' @param only_NODF just compute the NODF measurement of nestedness
+#' @return \code{calc_values} a list containing the following objects
+#' \itemize{
+#'  \item{\code{"graph"}}{ an \code{igraph::graph} object}
+#'  \item{\code{"max_xore"}}{ maximum k shell index}
+#'  \item{\code{"nested_values"}}{ a list containing all the values provided by the \code{bipartite::nested} function, unless \code{only_NODF} set \code{TRUE}}
+#'  \item{\code{"num_guild_a"}}{ number of nodes of guild a}
+#'  \item{\code{"num_guild_b"}}{ number of nodes of guild b}
+#'  \item{\code{"links"}}{ number of network links}
+#'  \item{\code{"meandist"}}{ network average kradius}
+#'  \item{\code{"meankdegree"}}{ network average kdegree}
+#'  \item{\code{"spaths_mat"}}{ matrix with node to node shortest distance paths}
+#'  \item{\code{"matrix"}}{ adyacency matrix with nodes of guild a by columns and guild b by rows}
+#'  \item{\code{"g_cores"}}{ list with the value of kshell for each node}
+#'  \item{\code{"modularity_measure"}}{ value of \code{igraph::modularity} function}
+#'  }
 #' @export
-#' @examples
-#' analyze_network()
+#' @examples result_analysis <- analyze_network("M_PL_007.csv", directory = "data/", guild_a = "Plant", guild_b = "Pollinator")
+
+
 analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b = "pol", plot_graphs = FALSE, only_NODF = FALSE)
 {
-  
+
   calc_kradius <- function(i)
   {
     kradius <- 0
@@ -104,10 +97,10 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
     V(an$g)[i]$kradius <- kradius
     V(an$g)[i]$guild <- an$guild
   }
-  
+
   an <<- new.env()
   # zinit_time <- proc.time()
-  
+
   nread <- read_network(namenetwork, directory = directory, guild_astr = guild_a, guild_bstr = guild_b)
   an$g <- as.undirected(nread$g)
   m <- nread$matrix
@@ -118,12 +111,12 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
   edge_matrix <- get.edges(an$g, E(an$g))
   spaths_mat <- shortest.paths(an$g)
   g_cores <- graph.coreness(an$g)
-  
+
   wtc <- walktrap.community(an$g)
   #modularity(wtc)
   modularity_measure <- modularity(an$g, membership(wtc))
-  
-  
+
+
   if (plot_graphs){
     plot(an$g, vertex.size=8, layout=layout.kamada.kawai)
     hist(g_cores,right=FALSE)
@@ -137,11 +130,11 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
     p[k] <- list(names(g_cores[g_cores == k]))
   }
   # Find plants and plos of a core
-  
+
   plants_k <- list(rep(NA,max_core))
   pols_k <- list(rep(NA,max_core))
   for (i in 1:max_core)
-  { 
+  {
     auxincore <- c(p[[i]][grep(guild_a,as.character(unlist(p[i])))])
     if (length(auxincore)>0){
       plants_k[[i]] <- auxincore
@@ -161,7 +154,7 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
   pols_maxcore <- p[[max_core]][grep(guild_b,as.character(unlist(p[max_core])))]
   # Purge possible nodes of kcore number maximum that are not part of the giant component
   # Only one case detected, when kcoremax == 2, network PL_30
-  
+
   if (max_core ==2)
   {
     meandiscnodes <- mean(spaths_mat== Inf)
@@ -175,8 +168,8 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
   V(an$g)$kradius <- NA
   V(an$g)$kcorenum <- NA
   V(an$g)$kdegree <- 0
-  
-  
+
+
   V(an$g)$guild <- ""
   E(an$g)$weights <- 1
   for (i in 1:max_core)
@@ -187,57 +180,29 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
         V(an$g)[k]$kcorenum <- i
     }
   }
-  
-  V(an$g)$krisk <- 0 # Cambiado JGA V(an$g)$kdegree
+
+  V(an$g)$krisk <- 0
   listanodos <- grep(guild_a,V(an$g)$name)
   an$guild <- guild_a
   an$guild_maxcore <- pols_maxcore
   lapply(listanodos, calc_kradius)
-  
+
   listanodos <- grep(guild_b,V(an$g)$name)
   an$guild <- guild_b
   an$guild_maxcore <- plants_maxcore
   lapply(listanodos, calc_kradius)
-  
-  
+
+
   meandist <- mean(V(an$g)$kradius[V(an$g)$kradius != Inf])
-  # Cambiado JGA nested_values<- nested(as.matrix(m), "ALL")
   if (only_NODF)
     nested_values<- nested(as.matrix(m), method = "NODF")
   else
     nested_values<- nested(as.matrix(m), "ALL")
-  #Fin cambio JGA
-  
+
   # kdegree computation
-  
   aux_graf <- data.frame(kdegree=V(an$g)$kdegree, kradius=V(an$g)$kradius ,
-                         krisk=V(an$g)$krisk, kcorenum=V(an$g)$kcorenum)
-  
-  # Cambio JGA. Modificado computo Krisk, puedes eliminar la secci???n comentada
-  #
-  #   for (l in 1:nrow(edge_matrix))
-  #   {
-  #     polvertex = edge_matrix[l,1]
-  #     plantvertex = edge_matrix[l,2]
-  #     aux_graf$kdegree[polvertex] = aux_graf$kdegree[polvertex] + 1/aux_graf$kradius[plantvertex]
-  #     aux_graf$kdegree[plantvertex] = aux_graf$kdegree[plantvertex] + 1/aux_graf$kradius[polvertex]
-  #     if (aux_graf$kradius[plantvertex] != Inf)
-  #       if (aux_graf$kcorenum[polvertex] > 1)
-  #         aux_graf$krisk[polvertex] = aux_graf$krisk[polvertex] + 
-  #       as.integer(aux_graf$kcorenum[plantvertex] < aux_graf$kcorenum[polvertex])*
-  #       (aux_graf$kcorenum[polvertex] - aux_graf$kcorenum[plantvertex])
-  #     else
-  #       
-  #       aux_graf$krisk[polvertex] = aux_graf$krisk[polvertex] + (aux_graf$kcorenum[plantvertex] == 1)
-  #     if (aux_graf$kradius[polvertex] != Inf)
-  #       if (aux_graf$kcorenum[plantvertex] > 1)
-  #         aux_graf$krisk[plantvertex] = aux_graf$krisk[plantvertex] +
-  #                                       as.integer(aux_graf$kcorenum[polvertex] < aux_graf$kcorenum[plantvertex])*(aux_graf$kcorenum[plantvertex] - aux_graf$kcorenum[polvertex])
-  #     else
-  #       aux_graf$krisk[plantvertex] = aux_graf$krisk[plantvertex] + (aux_graf$kcorenum[polvertex] == 1)
-  #   }  
-  #   Fin cambio JGA. Modificado el c???lculo de Krisk
-  
+                        krisk=V(an$g)$krisk, kcorenum=V(an$g)$kcorenum)
+
   for (l in 1:nrow(edge_matrix))
   {
     polvertex = edge_matrix[l,2]
@@ -245,25 +210,25 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
     diff_kcorenum = aux_graf$kcorenum[plantvertex] - aux_graf$kcorenum[polvertex]
     aux_graf$kdegree[polvertex] = aux_graf$kdegree[polvertex] + 1/aux_graf$kradius[plantvertex]
     aux_graf$kdegree[plantvertex] = aux_graf$kdegree[plantvertex] + 1/aux_graf$kradius[polvertex]
-    
+
     if ((aux_graf$kradius[polvertex] != Inf) & (diff_kcorenum < 0))
       if (aux_graf$kcorenum[polvertex] > 1)
         aux_graf$krisk[polvertex] = aux_graf$krisk[polvertex] - diff_kcorenum
-    
+
     if ((aux_graf$kradius[plantvertex] != Inf) & (diff_kcorenum > 0 ))
-      if (aux_graf$kcorenum[plantvertex] > 1) 
+      if (aux_graf$kcorenum[plantvertex] > 1)
         aux_graf$krisk[plantvertex] = aux_graf$krisk[plantvertex] + diff_kcorenum
-  }  
-  
-  V(an$g)$kdegree <- aux_graf$kdegree 
-  V(an$g)$kradius <- aux_graf$kradius                         
+  }
+
+  V(an$g)$kdegree <- aux_graf$kdegree
+  V(an$g)$kradius <- aux_graf$kradius
   V(an$g)$krisk <- aux_graf$krisk
   V(an$g)$kcorenum <- aux_graf$kcorenum
   meankdegree <- mean(V(an$g)$kdegree)
-  
-  
-  calc_values <- list("graph" = an$g, "max_core" = max_core, "nested_values" = nested_values, "num_guild_a" = num_guild_a, 
-                      "num_guild_b" = num_guild_b, "links" = length(V(an$g)), "meandist" = meandist, "meankdegree" = meankdegree, 
+
+
+  calc_values <- list("graph" = an$g, "max_core" = max_core, "nested_values" = nested_values, "num_guild_a" = num_guild_a,
+                      "num_guild_b" = num_guild_b, "links" = length(V(an$g)), "meandist" = meandist, "meankdegree" = meankdegree,
                       "spaths_mat" = spaths_mat, "matrix" = as.matrix(m), "g_cores" = g_cores, "modularity_measure" = modularity_measure)
   return(calc_values)
 }
@@ -271,12 +236,12 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
 
 #' Get bipartite plot
 #'
-#' 
+#'
 #' @param love Do you love cats? Defaults to TRUE.
 #' @keywords cats
 #' @export
 #' @examples
-#' get_bipartitr()
+#' get_bipartite()
 
 get_bipartite <- function(g, str_guild_a = "Plant", str_guild_b = "Pollinator", plot_graphs = FALSE)
 {
