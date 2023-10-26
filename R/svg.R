0f21d9ec58ee9e605aa69e69b922cf5b2401f982
jgalgarra <javier.algarra@u-tad.com>
2023-10-26 13:23:43 +0200

    Removed Tahoma font-family from ziggurat plot

--- R/svg.R
+++ R/svg.R
@@ -176,7 +176,7 @@ SVG<-function(scale_factor) {
       valignstr = "baseline"
     }
     result <- paste0(result, "style=\"text-anchor:",halignstr,";dominant-baseline:",valignstr,
-                     ";font-family:Tahoma;font-size:", size*this$font_scale_factor, "px;fill:", color, "\"")
+                     ";font-size:", size*this$font_scale_factor, "px;fill:", color, "\"")
     result <- paste0(result, ">\n")
 
     # tspan
