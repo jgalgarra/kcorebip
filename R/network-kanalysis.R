82a165df7a128a531449c85ab68e1879eb210ca8
jgalgarra <javier.algarra@u-tad.com>
2024-12-12 18:18:11 +0100

    Bipartite plot image in .png format

--- R/network-kanalysis.R
+++ R/network-kanalysis.R
@@ -39,7 +39,7 @@ library(ggplot2)
 analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b = "pol", plot_graphs = FALSE, only_NODF = FALSE,
                             weight_direction = "none")
 {
-
+  
   # K radius is the average distance to nodes of maximum k index of the opposite guild
   calc_kradius <- function(i)
   {
@@ -51,7 +51,7 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
     V(an$g)[i]$kradius <- kradius
     V(an$g)[i]$guild <- an$guild
   }
-
+  
   an <<- new.env()
   # Read interaction matrix file
   nread <- read_network(namenetwork, directory = directory, guild_astr = guild_a, guild_bstr = guild_b)
@@ -71,12 +71,12 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
   # k-core decompose the network
   #g_cores <- graph.coreness(an$g)
   g_cores <- coreness(an$g)
-
+  
   #wtc <- walktrap.community(an$g)
   wtc <- cluster_walktrap(an$g)
   #modularity(wtc)
   modularity_measure <- modularity(an$g, membership(wtc))
-
+  
   # This option to plot graphs is only useful for a preliminary exam
   # becasuse quality is not good enough
   if (plot_graphs){
@@ -121,19 +121,19 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
     for (i in plants_maxcore)
       if (mean(spaths_mat[i,] == Inf)>2*meandiscnodes)
         plants_maxcore <- plants_maxcore[plants_maxcore!=i]
-      for (i in pols_maxcore)
-        if (mean(spaths_mat[i,] == Inf)>2*meandiscnodes)
-          pols_maxcore <- pols_maxcore[pols_maxcore!=i]
+    for (i in pols_maxcore)
+      if (mean(spaths_mat[i,] == Inf)>2*meandiscnodes)
+        pols_maxcore <- pols_maxcore[pols_maxcore!=i]
   }
   # Nodes that are not part of the Giant Component
   V(an$g)$kradius <- NA
   V(an$g)$kcorenum <- NA
   V(an$g)$kdegree <- 0
-
-
+  
+  
   V(an$g)$guild <- ""
   E(an$g)$weights <- 1
-
+  
   # Weight of links
   for(i in 1:length(E(an$g))){
     E(an$g)$weights[i] <- m[edge_matrix[i,][2]- num_guild_a,edge_matrix[i,][1]]
@@ -147,32 +147,32 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
         V(an$g)[k]$kcorenum <- i
     }
   }
-
+  
   # k-risk initialization
   V(an$g)$krisk <- 0
-
+  
   # k-radius computation
   listanodos <- grep(guild_a,V(an$g)$name)
   an$guild <- guild_a
   an$guild_maxcore <- pols_maxcore
   lapply(listanodos, calc_kradius)
-
+  
   listanodos <- grep(guild_b,V(an$g)$name)
   an$guild <- guild_b
   an$guild_maxcore <- plants_maxcore
   lapply(listanodos, calc_kradius)
-
+  
   meandist <- mean(V(an$g)$kradius[V(an$g)$kradius != Inf])
   # Computation of nestedness measures
   if (only_NODF)
     nested_values<- nested(as.matrix(m), method = "NODF")
   else
     nested_values<- nested(as.matrix(m), "ALL")
-
+  
   # kdegree computation
   aux_graf <- data.frame(kdegree=V(an$g)$kdegree, kradius=V(an$g)$kradius ,
                          krisk=V(an$g)$krisk, kcorenum=V(an$g)$kcorenum)
-
+  
   # k-risk computation
   for (l in 1:nrow(edge_matrix))
   {
@@ -181,23 +181,23 @@ analyze_network <- function(namenetwork, directory="", guild_a = "pl", guild_b =
     diff_kcorenum = aux_graf$kcorenum[plantvertex] - aux_graf$kcorenum[polvertex]
     aux_graf$kdegree[polvertex] = aux_graf$kdegree[polvertex] + 1/aux_graf$kradius[plantvertex]
     aux_graf$kdegree[plantvertex] = aux_graf$kdegree[plantvertex] + 1/aux_graf$kradius[polvertex]
-
+    
     if ((aux_graf$kradius[polvertex] != Inf) & (diff_kcorenum < 0))
       if (aux_graf$kcorenum[polvertex] > 1)
         aux_graf$krisk[polvertex] = aux_graf$krisk[polvertex] - diff_kcorenum
-
+    
     if ((aux_graf$kradius[plantvertex] != Inf) & (diff_kcorenum > 0 ))
       if (aux_graf$kcorenum[plantvertex] > 1)
         aux_graf$krisk[plantvertex] = aux_graf$krisk[plantvertex] + diff_kcorenum
   }
-
+  
   V(an$g)$kdegree <- aux_graf$kdegree
   V(an$g)$kradius <- aux_graf$kradius
   V(an$g)$krisk <- aux_graf$krisk + 0.01*aux_graf$kcorenum
   V(an$g)$kcorenum <- aux_graf$kcorenum
   meankdegree <- mean(V(an$g)$kdegree)
-
-
+  
+  
   calc_values <- list("graph" = an$g, "max_core" = max_core, "nested_values" = nested_values, "num_guild_a" = num_guild_a,
                       "num_guild_b" = num_guild_b, "links" = length(E(an$g)), "meandist" = meandist, "meankdegree" = meankdegree,
                       "spaths_mat" = spaths_mat, "matrix" = as.matrix(m), "g_cores" = g_cores, "modularity_measure" = modularity_measure)
@@ -255,10 +255,10 @@ read_network <- function(namenetwork, guild_astr = "pl", guild_bstr = "pol", dir
   namesred <- read.csv(paste0(directory,namenetwork),header=FALSE,stringsAsFactors=FALSE)
   names_guild_a <- unname(unlist(namesred[1,2:ncol(namesred)])) # JULY 2023 
   names_guild_b <- namesred[2:nrow(namesred),1]
-
+  
   #Reading matrix data
   m <- read.csv(paste0(directory,namenetwork),header=TRUE,row.names=1)
-
+  
   # Calc number of species of each guild
   num_guild_a <- ncol(m)
   num_guild_b <- nrow(m)
@@ -272,7 +272,7 @@ read_network <- function(namenetwork, guild_astr = "pl", guild_bstr = "pol", dir
   for (i in 1:num_guild_b){
     g <- g + vertices(paste0(guild_bstr,i),color="red",guild_id="b",name_species=names_guild_b[i],id=i)
   }
-
+  
   # Adding links to the graph object
   mm <- matrix(unlist(list(m)),nrow=num_guild_b,ncol=num_guild_a)
   listedgesn <- which(mm!=0, arr.ind = T)
@@ -285,7 +285,7 @@ read_network <- function(namenetwork, guild_astr = "pl", guild_bstr = "pol", dir
   calc_values <- list("graph" = g, "matrix" = m, "num_guild_b" = num_guild_b, "num_guild_a" = num_guild_a,
                       "names_guild_a" = names_guild_a, "names_guild_b"=names_guild_b)
   return(calc_values)
-
+  
 }
 
 #' Reads and analyzes a network from a file
@@ -338,4 +338,3 @@ read_and_analyze <- function(directorystr,network_file,label_strguilda,label_str
 
 # EXAMPLE. Cut and paste to test.
 #result_analysis <- analyze_network("M_PL_011.csv", directory = "../data/", guild_a = "Plant", guild_b = "Pollinator", plot_graphs = FALSE)
-
